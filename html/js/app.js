9eaa358d06616793aae66d288ef9e91b4bcdb5ee
Artem Andreev <arthique@ya.ru>
2016-07-20 14:46:47 +0400

    The page adapted for displays sizes 640 pixels and less. In common.js added modernizr, also added base JS functions.

--- html/js/app.js
+++ html/js/app.js
@@ -1,3 +1,31 @@
-function isTouchDevice(){
-	return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
-}
+"use strict";
+(function() {
+	var app = app || {};
+	app.init = function(param){
+		var initStatus = true;
+		if(initStatus){
+			app.consts = {};
+			app.names = {};
+			app.vars = {};
+			app.names.button = "button";	
+		}
+		if(typeof param == 'function'){
+			app[param.name] = param;
+		} else {
+			// todo init(add) app's custom property
+		}
+		initStatus = false;
+	};
+	app.isTouchDevice = function() {
+		return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+	}
+	window.app = app;
+	app.init();
+})(window);
+
+(function() {
+	var isTest = function(){
+		console.log('test');
+	}
+	app.init(isTest);
+})(app);
